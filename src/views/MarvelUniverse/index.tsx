3850fe549274f973af172d94a951dca04f8ece6d
Zafer Gok <zfrgk96@gmail.com>
2022-07-11 02:35:27 +0300

    created className

--- src/views/MarvelUniverse/index.tsx
+++ src/views/MarvelUniverse/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 type Props = {};
 
 function MarvelUniverse({}: Props) {
-  return <div>MarvelUniverse</div>;
+  return <h1 className="text-3xl font-bold underline">MarvelUniverse</h1>;
 }
 
 export default MarvelUniverse;
