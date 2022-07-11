0745392c4d16e6a98b23be32627441eac14ab8f4
Zafer Gok <zfrgk96@gmail.com>
2022-07-11 03:42:26 +0300

    created routes

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AllRoutes from "./route";
+
+type Props = {};
+
+function App({}: Props) {
+  return (
+    <div>
+      <AllRoutes />
+    </div>
+  );
+}
+
+export default App;
